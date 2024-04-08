fadd0f13315b192d16844ae69e7594f9eb83c326
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-04-07 19:04:52 -0700

    Update App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">React Search</header>
+      <header className="App-header">React Search Application</header>
     </div>
   );
 }
