41f75ad6c50878c041f9ec431f85e58bb6f4c392
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-04-10 20:49:16 -0700

    Use useState hook in component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("React Search Application");
   return (
     <div className="App">
-      <header className="App-header">React Search Application</header>
+      <header className="App-header">{name}</header>
     </div>
   );
 }
