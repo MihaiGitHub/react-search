e88cb931019609c251b7722c3f4d46dae95b9476
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-04-11 11:40:22 -0700

    Add event handler

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("React Search Application");
+
+  const handleClick = () => {
+    setName("Changed");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">{name}</header>
+      <header className="App-header">
+        <button onClick={() => handleClick()}>{name}</button>
+      </header>
     </div>
   );
 }
